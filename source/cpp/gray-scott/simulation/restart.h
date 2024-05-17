f3774ffe259ad028caad33a0bab953f5141c925d
zauser519 <56807402+zauser519@users.noreply.github.com>
2024-05-17 15:50:25 +0900

    Update restart.h

--- source/cpp/gray-scott/simulation/restart.h
+++ source/cpp/gray-scott/simulation/restart.h
@@ -7,9 +7,9 @@
 #include <adios2.h>
 #include <mpi.h>
 
-void WriteCkpt(MPI_Comm comm, const int step, const Settings &settings,
-               const GrayScott &sim, adios2::IO io);
-int ReadRestart(MPI_Comm comm, const Settings &settings, GrayScott &sim,
-                adios2::IO io);
+//void WriteCkpt(MPI_Comm comm, const int step, const Settings &settings,
+//               const GrayScott &sim, adios2::IO io);
+//int ReadRestart(MPI_Comm comm, const Settings &settings, GrayScott &sim,
+//                adios2::IO io);
 
 #endif
