b83325ff460a6fe80150a97cec63ceb56552cec5
zauser519 <56807402+zauser519@users.noreply.github.com>
2024-05-17 15:19:22 +0900

    Delete gray-scott directory

--- gray-scott/catalyst/setup.sh
+++
@@ -1,6 +0,0 @@
-# The ${PARAVIEW_BUILD_PATH}/lib directory should contain `libADIOSInSituPlugin.so`.
-export PARAVIEW_BUILD_PATH=/home/adios/Software/paraview/build
-# Set the following env variables.
-export ADIOS2_PLUGIN_PATH=${PARAVIEW_BUILD_PATH}/lib
-export CATALYST_IMPLEMENTATION_NAME=paraview
-export CATALYST_IMPLEMENTATION_PATHS=${PARAVIEW_BUILD_PATH}/lib/catalyst
